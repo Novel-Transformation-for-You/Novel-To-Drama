91afc1cad4f5f0c741b902a8dc53ec2dc72d281a
SeungHoJUN <89953442+SeungHoJUN@users.noreply.github.com>
2023-12-13 17:18:44 +0900

    sh 파일 수정

--- CSN-SAPR-다중작품/run_train.sh
+++ CSN-SAPR-다중작품/run_train.sh
@@ -17,12 +17,14 @@ CUDA_VISIBLE_DEVICES=0 python train.py \
 --patience 10 \
 --bert_pretrained_dir ${BERT_PRETRAINED_DIR} \
 --train_file \
-${DATA_PREFIX}/train/train_unsplit_symbol1.txt \
+${DATA_PREFIX}/train/train_unsplit_symbol3.txt \
 --dev_file \
-${DATA_PREFIX}/dev/dev_unsplit_symbol1.txt \
+${DATA_PREFIX}/dev/dev_unsplit_symbol3.txt \
 --test_file \
-${DATA_PREFIX}/test/test_unsplit_symbol1.txt \
+${DATA_PREFIX}/test/test_unsplit_symbol3.txt \
 --name_list_path \
 ${DATA_PREFIX}/name_list_symbol.txt \
+--namelist_file \
+${DATA_PREFIX}/name_list_per_novel1.json \
 --length_limit 510 \
 # --checkpoint_dir ${CHECKPOINT_DIR}
